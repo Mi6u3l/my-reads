b5ad5d8b8a6b7eb08f091ee0c06b7e26e21dd208
Mi6u3l <miguelbragagomes@gmail.com>
2018-02-11 13:10:20 +0000

    feat:add shelves

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="list-books-title">
-          <h1 className="App-title">My Reads</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return;
+    <div className="App">
+      <header className="list-books-title">
+        <h1 className="App-title">My Reads</h1>
+      </header>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
       </div>
-    );
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+      </div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+      </div>
+    </div>;
   }
 }
 
