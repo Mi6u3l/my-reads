1b94b30b2b3e87fb7ea54a96fb0672215cb2c4b3
Mi6u3l <miguelbragagomes@gmail.com>
2018-02-11 13:20:19 +0000

    feat:shelve as component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Shelve from "./Shelve";
 
 class App extends Component {
   render() {
-    return;
-    <div className="App">
-      <header className="list-books-title">
-        <h1 className="App-title">My Reads</h1>
-      </header>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+    return (
+      <div className="App">
+        <header className="list-books-title">
+          <h1 className="App-title">My Reads</h1>
+        </header>
+        <Shelve shelveName="Currenlty Reading" />
+        <Shelve shelveName="Want to Read" />
+        <Shelve shelveName="Read" />
       </div>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Want to Read</h2>
-      </div>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Read</h2>
-      </div>
-    </div>;
+    );
   }
 }
 
